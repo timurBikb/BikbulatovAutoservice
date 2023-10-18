adbb9e9a23b463909beb1df4e4515d631acc916e
sparkle-iden <ertyertyer@gmail.com>
2023-10-18 13:31:45 +0500

    лаба 7

--- BikbulatovAutoservice/ServicePage.xaml.cs
+++ BikbulatovAutoservice/ServicePage.xaml.cs
@@ -20,6 +20,12 @@ namespace BikbulatovAutoservice
     /// </summary>
     public partial class ServicePage : Page
     {
+        int CountRecords; // количество записей в таблице
+        int CountPage; // общее количество страниц
+        int CurrentPage = 0; // Текущая страница
+
+        List<Service> CurrentPageList = new List<Service>();
+        List<Service> TableList;
         public ServicePage()
         {
             InitializeComponent();
@@ -81,7 +87,113 @@ namespace BikbulatovAutoservice
             }
             // отображаем итоги поиска/фильтрации/сортировки
             ServiceListView.ItemsSource = currentServices;
+            // заполнение таблицы для постраничного вывода
+            TableList = currentServices;
+            // вызов функции отображения кол-ва страниц с параметрами:
+            // направление 0 - начальная загрузка
+            // 0 - выбранная страница
+            ChangePage(0, 0);
         }
+
+        private void ChangePage(int direction, int? selectedPage) // Функция отвечающая за разделение list'а
+        {
+            // direction - направление. 0 - начало, 1 - предыдущая страница, 2 - следующая страница
+            // selectedPage - при нажатии на стрелочки передается null,
+            // при выборе определенной страницы в этой переенной находится номер страницы
+
+            CurrentPageList.Clear(); // начальная очистка листа
+            CountRecords = TableList.Count; // определение количества записей во всем списке
+            // определение количества страниц
+            if (CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+
+            Boolean Ifupdate = true;
+            // Проверка на правильность - если
+            // CurrentPage(номер текущей страницы) "правильный"
+
+            int min;
+
+            if (selectedPage.HasValue) // проверка на значение не null (тк может быть null)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else // если нажата стрелка
+            {
+                switch (direction)
+                {
+                    case 1: // нажата кнопка "Предыдущая страница"
+                        if (CurrentPage > 0)
+                        // то есть кнопка нажата правильно и "назад" можно идти
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                            // в случае если currentPage попытается выйти из диапазона
+                            // внесение данных не произойдет
+                        }
+                        break;
+
+                    case 2: // нажат кнопка "следующая страница"
+                        if (CurrentPage < CountPage - 1)
+                        // если вперед идти можно
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                }
+            }
+                if (Ifupdate) // если currentPage не вышел из диапазона, то
+                {
+                    PageListBox.Items.Clear();
+                    // Удаление старых значений из listBox'a номеров страниц, нужно чтобы при изменении
+                    // количества записей кол-во страниц динамически изменялось
+                    for (int i = 1; i <= CountPage; i++)
+                    {
+                        PageListBox.Items.Add(i);
+                    }
+                    PageListBox.SelectedIndex = CurrentPage;
+
+                    // вывод количества записей на странице и общего количества
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    TBCount.Text = min.ToString();
+                    TBallRecords.Text = " из " + CountRecords.ToString();
+
+                    ServiceListView.ItemsSource = CurrentPageList;
+                    // обновить отображение списка услуг
+                    ServiceListView.Items.Refresh();
+                }
+            }
+        
 
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -144,6 +256,54 @@ namespace BikbulatovAutoservice
             }
 
             UpdateServices();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            // забираем сервис, для которого нажата кнопка удалить
+            var currentService = (sender as Button).DataContext as Service;
+
+            // проверка на возможность удаления
+            var currentClientServices = Bikbulatov_autoserviceEntities.GetContext().ClientService.ToList();
+            currentClientServices = currentClientServices.Where(p => p.ServiceID == currentService.ID).ToList();
+
+            if (currentClientServices.Count != 0) // если есть записи на этот сервис
+                MessageBox.Show("Невозможно выполнить удаление, так как существуют записи на эту услугу");
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Bikbulatov_autoserviceEntities.GetContext().Service.Remove(currentService);
+                        Bikbulatov_autoserviceEntities.GetContext().SaveChanges();
+                        // выводим в листвью измененную таблицу Сервис
+                        ServiceListView.ItemsSource = Bikbulatov_autoserviceEntities.GetContext().Service.ToList();
+                        // чтобы применились фильтры и поиск, если они были на форме изначально
+                        UpdateServices();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
         }
 
         /*
